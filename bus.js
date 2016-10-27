2eaca49024286e23d882f0c019683992880c8e47
Bartosz Rumiński <bartoshx@gmail.com>
2016-10-27 16:04:57 +0200

    Faster switch

--- bus.js
+++ bus.js
@@ -1,45 +1,49 @@
 var id = "home";
 var timetable;
+var timetables = {};
 
 window.onload = function() {
-	// handleRefresh();
-	// setInterval(handleRefresh, 3000);
-	fetchData();
+	fetchData("home");
+	fetchData("work");
 }
 
-function loadTimetable(data){
-	timetable = data.timetable;
-	var index = data.timetable.findIndex(notPast);
-    createList(timetable, index, 3);
-	
-    var directionTag = document.getElementById('direction');
-    directionTag.innerHTML = data.title;
-	
-	setInterval(update,  500);
+function onDataLoaded(data){
+	timetables[data.tag] = {timetable: data.timetable, title: data.title};
+	console.log(Object.keys(timetables).length);
+		
+	if (Object.keys(timetables).length == 2) {
+		timetable = timetables[id].timetable;
+		update();
+    	setDirection(timetables[id].title);
+		setInterval(update,  500);
+	}
 }
 
 function onDirectionClick(){
    id = (id == "work")? "home" : "work";
-   fetchData();
+   update();
 }
 
-function getDirection(){
-    return document.getElementById('path').innerHTML;
+function setDirection(title){
+    var directionTag = document.getElementById('direction');
+    directionTag.innerHTML = title;
  }
  
  function update(){
+	timetable = timetables[id].timetable;
  	var index = timetable.findIndex(notPast);
+	setDirection(timetables[id].title);
     createList(timetable, index, 3);
+	setTitle(index);
  }
  
- function notPast(item){
-	 var d = new Date();
-	 return (d.getMinutes()+(60*d.getHours())) <= (item.minute+(60*item.hour));
+ function setTitle(n){
+	 var left = remains(timetable[n].hour, timetable[n].minute)
+	 document.title = "Bus / "+left+" min";
  }
 
-function fetchData(){
-	var url = "https://script.google.com/macros/s/AKfycbxLfPvkUE6x_ROTRVU_UjlZojvj71TYJdWonQj_dFc/exec?callback=loadTimetable&direction="+id;
-		
+function fetchData(id){
+	var url = "https://script.google.com/macros/s/AKfycbxLfPvkUE6x_ROTRVU_UjlZojvj71TYJdWonQj_dFc/exec?callback=onDataLoaded&direction="+id;	
 	var newScript = document.createElement("script");
 	newScript.setAttribute("src", url);
 	newScript.setAttribute("id","jsonp");
@@ -53,17 +57,8 @@ function fetchData(){
 	}
 }
 
-function remains(hour,min){
-  var d = new Date();
-  var nhour = d.getHours()
-  var nmin = d.getMinutes();
-  if(nhour == hour) {
-          return (min-nmin);
-    } else {
-          var dhour = Math.abs(hour-nhour);
-          return ((dhour*60)-nmin)+min;
-   }
-}
+
+// Function for List
 
 function createList(schedule, index, count){
      var ul = document.getElementById("list");
@@ -89,8 +84,30 @@ function clearList(){
       } 
 }
 
+
+// Helper functions 
+
  // add 0 to numbers below 10
  function formatMinutes(deg){
    if(deg>=10) return deg;
    return ('0' + deg).slice(-2);
+}
+
+// false for past arrivals, true otherwise 
+function notPast(item){
+ var d = new Date();
+ return (d.getMinutes()+(60*d.getHours())) <= (item.minute+(60*item.hour));
+}
+
+// return remain time to arrival in minutes
+function remains(hour,min){
+  var d = new Date();
+  var nhour = d.getHours()
+  var nmin = d.getMinutes();
+  if(nhour == hour) {
+          return (min-nmin);
+    } else {
+          var dhour = Math.abs(hour-nhour);
+          return ((dhour*60)-nmin)+min;
+   }
 }
