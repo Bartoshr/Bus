c00aa913126006f5368fc141c8140f79c81fffeb
Bartosz Ruminski <bartosz.ruminski@intern.allegrogroup.com>
2017-08-22 15:57:01 +0200

    Caching

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
+  '/',
   'bus.css',
-  'bus.js'
+  'bus.js',
+  'https://script.google.com/macros/s/AKfycbxHXrx9YLhONoVk9ZXz9YrvMVJhkk-qI7RwRZv3EM8DYdObrqc/exec?callback=onDataLoaded&direction=home',
+  'https://script.google.com/macros/s/AKfycbxHXrx9YLhONoVk9ZXz9YrvMVJhkk-qI7RwRZv3EM8DYdObrqc/exec?callback=onDataLoaded&direction=work',
 ];
 
 self.addEventListener('install', function(event) {
@@ -17,24 +20,23 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
 	
-	if (event.request.url.endsWith('.worker')) {
-	    event.respondWith(new Response('<strong>Ten URL istnieje!</strong>',
-	    {headers:
-	     {"Content-type":"text/html"}
-	   }));
-	  }
+	if (event.request.url.endsWith('version')) {
+	    event.respondWith(new Response('<strong>Version 0.9</strong>',
+      {headers:{"Content-type":"text/html"}}));
+      return ;
+	}
 
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
-        // Cache hit - return response
-		  
         if (response) {
+          console.log("Cache: " + event.request.method + " " + event.request.url);
           return response;
         }
-		console.log("Fetch " + response);
+        console.log("Fetch: " + event.request.method + " " + event.request.url);
         return fetch(event.request);
       }
     )
   );
+
 });
