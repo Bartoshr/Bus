4cd5c31fd0e1f780d75aa620f7ed153ebda774e9
Bartosz Rumiński <bartosh.ruminski@itcraft.pl>
2019-05-03 23:11:08 +0200

    Reformat Code

--- bus.js
+++ bus.js
@@ -12,67 +12,67 @@ var shift = 0;
 // index of bus arrivel which should be diplayed in notification
 var notifyIndex = -1;
 
-Number.prototype.mod = function(n) {
+Number.prototype.mod = function (n) {
 	return ((this % n) + n) % n;
 };
 
-window.onload = function() {
-	
+window.onload = function () {
+
 	fetchData('home');
 	fetchData('work');
-	
+
 	if ('serviceWorker' in navigator) {
-	     navigator.serviceWorker.register('sw.js').then(function(registration) {
-	      // Registration was successful
-	      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-	    }, function(err) {
-	      // registration failed :(
-	      console.log('ServiceWorker registration failed: ', err);
-	    });
+		navigator.serviceWorker.register('sw.js').then(function (registration) {
+			// Registration was successful
+			console.log('ServiceWorker registration successful with scope: ', registration.scope);
+		}, function (err) {
+			// registration failed :(
+			console.log('ServiceWorker registration failed: ', err);
+		});
 	}
 };
 
 function onDataLoaded(data) {
-	timetables[data.tag] = {timetable: data.timetable, title: data.title};
+	timetables[data.tag] = { timetable: data.timetable, title: data.title };
 
 	if (Object.keys(timetables).length == 2) {
 		timetable = timetables[id].timetable;
 		update();
-    	setDirection(timetables[id].title);
+		setDirection(timetables[id].title);
 		setInterval(update, 500);
 	}
 }
 
 function onDirectionClick() {
-   id = (id == 'work') ? 'home' : 'work';
-   shift = 0;
-   notifyIndex = -1;
-   update();
+	id = (id == 'work') ? 'home' : 'work';
+	shift = 0;
+	notifyIndex = -1;
+	update();
 }
 
 function setDirection(title) {
-    var directionTag = document.getElementById('direction');
-    directionTag.innerHTML = title;
- }
+	var directionTag = document.getElementById('direction');
+	directionTag.innerHTML = title;
+}
 
- function update() {
+function update() {
 	timetable = timetables[id].timetable;
- 	index = timetable.findIndex(notPast);
+	index = timetable.findIndex(notPast);
 	if (index == -1) index = 1;
 
 	setDirection(timetables[id].title);
-    createList(timetable, index, 3);
+	createList(timetable, index, 3);
 	setTitle(index);
 	showNotification(index);
- }
+}
 
- function setTitle(n) {
-	 var left = remains(timetable[n].hour, timetable[n].minute);
-	 document.title = 'Bus / '+ left + ' min';
- }
+function setTitle(n) {
+	var left = remains(timetable[n].hour, timetable[n].minute);
+	document.title = 'Bus / ' + left + ' min';
+}
 
 function fetchData(id) {
-	var url = 'https://script.google.com/macros/s/AKfycbxHXrx9YLhONoVk9ZXz9YrvMVJhkk-qI7RwRZv3EM8DYdObrqc/exec?callback=onDataLoaded&direction='+ id;
+	var url = 'https://script.google.com/macros/s/AKfycbxHXrx9YLhONoVk9ZXz9YrvMVJhkk-qI7RwRZv3EM8DYdObrqc/exec?callback=onDataLoaded&direction=' + id;
 	var newScript = document.createElement('script');
 	newScript.setAttribute('src', url);
 	newScript.setAttribute('id', 'jsonp');
@@ -94,20 +94,20 @@ function onItemClick(item) {
 }
 
 function onItemLongPress(item) {
-	var localIndex = 0;
-	if (item.target.getAttribute('id') == 'first') localIndex = 0;
-	if (item.target.getAttribute('id') == 'last') localIndex = 2;
-	if (item.target.getAttribute('id') == 'middle') localIndex = 1;
-	notifyIndex = index+localIndex+shift;
-	update();
+	// var localIndex = 0;
+	// if (item.target.getAttribute('id') == 'first') localIndex = 0;
+	// if (item.target.getAttribute('id') == 'last') localIndex = 2;
+	// if (item.target.getAttribute('id') == 'middle') localIndex = 1;
+	// notifyIndex = index+localIndex+shift;
+	// update();
 }
 
 function showNotificationOnTime(n) {
 	var left = remains(timetable[n].hour, timetable[n].minute);
 	if (left <= 10 && notifyIndex == n) {
 		if (window.Notification && Notification.permission !== 'denied') {
-			Notification.requestPermission(function(status) {  // status is "granted", if accepted by user
-				var n = new Notification('Bus / '+ left + ' min', {
+			Notification.requestPermission(function (status) {  // status is "granted", if accepted by user
+				var n = new Notification('Bus / ' + left + ' min', {
 					body: 'Hurry up',
 					icon: 'icons/ic_launcher_96.png' // optional
 				});
@@ -120,15 +120,15 @@ function showNotificationOnTime(n) {
 function showNotification(n) {
 	var left = remains(timetable[n].hour, timetable[n].minute);
 	if (left <= 10 && notifyIndex == n) {
-		Notification.requestPermission(function(result) {
-		  if (result === 'granted') {
-		    navigator.serviceWorker.ready.then(function(registration) {
-		      registration.showNotification('Bus / '+ left + ' min', {
-		        icon: 'icons/ic_launcher_96.png',
-				badge: 'icons/ic_launcher_96.png'
-		      });
-		    });
-		  }
+		Notification.requestPermission(function (result) {
+			if (result === 'granted') {
+				navigator.serviceWorker.ready.then(function (registration) {
+					registration.showNotification('Bus / ' + left + ' min', {
+						icon: 'icons/ic_launcher_96.png',
+						badge: 'icons/ic_launcher_96.png'
+					});
+				});
+			}
 		});
 		notifyIndex = -1;
 	}
@@ -139,73 +139,73 @@ function showNotification(n) {
 // Function for List
 
 function createList(schedule, index, count) {
-     var ul = document.getElementById('list');
-     clearList();
-     for (i = index + shift; i < index + count + shift; i++) {
-          var text = '';
-          var li = document.createElement('li');
-		  var n = i.mod(schedule.length);
-          text += schedule[n].hour + ':'+ formatMinutes(schedule[n].minute);
+	var ul = document.getElementById('list');
+	clearList();
+	for (i = index + shift; i < index + count + shift; i++) {
+		var text = '';
+		var li = document.createElement('li');
+		var n = i.mod(schedule.length);
+		text += schedule[n].hour + ':' + formatMinutes(schedule[n].minute);
 
-          if (index + shift >= index) text += ' /'+ formatMinutes(remains(schedule[n].hour, schedule[n].minute));
-		  if (index + shift < index) text += ' /'+ formatMinutes(past(schedule[n].hour, schedule[n].minute));
+		if (index + shift >= index) text += ' /' + formatMinutes(remains(schedule[n].hour, schedule[n].minute));
+		if (index + shift < index) text += ' /' + formatMinutes(past(schedule[n].hour, schedule[n].minute));
 
-          li.appendChild(document.createTextNode(text));
-		  li.addEventListener('click', onItemClick)
-		  li.addEventListener('mousedown', onItemLongPressDisp);
-		  li.addEventListener('mouseup', revert);
-		  li.addEventListener('touchstart', onItemLongPressDisp);
-		  li.addEventListener('touchend', revert);
-		  li.addEventListener('touchcancel', revert);
+		li.appendChild(document.createTextNode(text));
+		li.addEventListener('click', onItemClick)
+		li.addEventListener('mousedown', onItemLongPressDisp);
+		li.addEventListener('mouseup', revert);
+		li.addEventListener('touchstart', onItemLongPressDisp);
+		li.addEventListener('touchend', revert);
+		li.addEventListener('touchcancel', revert);
 
-		  if (i == index) li.setAttribute('class', 'marked');
-          if (i == index + shift) li.setAttribute('id', 'first');
-		  if (i > index + shift && i < index + count + shift - 1) li.setAttribute('id', 'middle');
-          if (i == index + count + shift - 1) li.setAttribute('id', 'last');
-		  if (i == notifyIndex) li.setAttribute('class', 'ringing');
+		if (i == index) li.setAttribute('class', 'marked');
+		if (i == index + shift) li.setAttribute('id', 'first');
+		if (i > index + shift && i < index + count + shift - 1) li.setAttribute('id', 'middle');
+		if (i == index + count + shift - 1) li.setAttribute('id', 'last');
+		if (i == notifyIndex) li.setAttribute('class', 'ringing');
 
-          ul.appendChild(li);
-       }
+		ul.appendChild(li);
+	}
 }
 
 function clearList() {
-   var myNode = document.getElementById('list');
-     while (myNode.firstChild) {
-        myNode.removeChild(myNode.firstChild);
-      }
+	var myNode = document.getElementById('list');
+	while (myNode.firstChild) {
+		myNode.removeChild(myNode.firstChild);
+	}
 }
 
 
 // Helper functions
 
- // add 0 to numbers below 10
- function formatMinutes(deg) {
-   if (deg >= 10 || deg < 0) return deg;
-   return ('0' + deg).slice(-2);
+// add 0 to numbers below 10
+function formatMinutes(deg) {
+	if (deg >= 10 || deg < 0) return deg;
+	return ('0' + deg).slice(-2);
 }
 
 // false for past arrivals, true otherwise
 function notPast(item) {
- var d = new Date();
- return (d.getMinutes() + (60 * d.getHours())) <= (item.minute + (60 * item.hour));
+	var d = new Date();
+	return (d.getMinutes() + (60 * d.getHours())) <= (item.minute + (60 * item.hour));
 }
 
 // return remain time to arrival in minutes
 
 function past(hour, min) {
-    var d = new Date();
-    var nhour = d.getHours();
-    var nmin = d.getMinutes();
-    return ((hour * 60) + min) - ((nhour * 60) + nmin);
+	var d = new Date();
+	var nhour = d.getHours();
+	var nmin = d.getMinutes();
+	return ((hour * 60) + min) - ((nhour * 60) + nmin);
 }
 
 function remains(hour, min) {
-  var d = new Date();
-  var nhour = d.getHours();
-  var nmin = d.getMinutes();
-  if (((hour * 60) + min) >= ((nhour * 60) + nmin))
-  return ((hour * 60) + min) - ((nhour * 60) + nmin);
-  else return (24 * 60 - ((nhour * 60) + nmin)) + ((hour * 60) + min);
+	var d = new Date();
+	var nhour = d.getHours();
+	var nmin = d.getMinutes();
+	if (((hour * 60) + min) >= ((nhour * 60) + nmin))
+		return ((hour * 60) + min) - ((nhour * 60) + nmin);
+	else return (24 * 60 - ((nhour * 60) + nmin)) + ((hour * 60) + min);
 }
 
 // Long Press Helper Functions
@@ -213,21 +213,18 @@ function remains(hour, min) {
 var timer;
 var istrue = false;
 var delay = 1000; // how much long u have to hold click in MS
-function onItemLongPressDisp(item)
-{
-   istrue = true;
-   timer = setTimeout(function(){ 
-       if(timer)
-       clearTimeout(timer);   
-       if(istrue)
-       {
-           onItemLongPress(item);
-       }
-   }, delay);
+function onItemLongPressDisp(item) {
+	istrue = true;
+	timer = setTimeout(function () {
+		if (timer)
+			clearTimeout(timer);
+		if (istrue) {
+			onItemLongPress(item);
+		}
+	}, delay);
 }
 
-function revert()
-{
-   istrue = false;
+function revert() {
+	istrue = false;
 }
 
